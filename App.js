a463353978f9ed21369e02a38e03212d2801de55
Isabella <isabella.cabrera@aluno.senai.br>
2026-02-13 08:29:42 -0300

    chore: initialize project with package.json and essential dependencies

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>PPDM</Text>
+      <Text>A melhor aula de DS!😍😒😒</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
